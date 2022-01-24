cfd37b5fef6c1ebbbb502c7888f7804d52c1410a
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2022-01-24 15:30:36 -0600

    Initial Commit

---
+++ firestore-demo-spm/firestore_demo_spmApp.swift
@@ -0,0 +1,17 @@
+//
+//  firestore_demo_spmApp.swift
+//  firestore-demo-spm
+//
+//  Created by Ivette Fernandez on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct firestore_demo_spmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
